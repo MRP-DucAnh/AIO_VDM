f8bbff785ae9c73c802cc4868e5264c318173855
shiba <shiba.spj@hotmail.com>
2026-04-19 18:19:40 +0530

    Refactor IntentUtility to use coroutines for link opening
    Update `openLinkInSystemBrowser` to be a `suspend` function and ensure it executes on the main thread.
    
    - Convert `openLinkInSystemBrowser` to a `suspend` function.
    - Wrap the link validation and intent launching logic within a `withMainContext` block to ensure UI-related operations are thread-safe.

--- app/src/main/java/lib/device/IntentUtility.kt
+++ app/src/main/java/lib/device/IntentUtility.kt
@@ -39,24 +39,26 @@ object IntentUtility {
 	}
 
 	@JvmStatic
-	fun openLinkInSystemBrowser(context: Context,
-	                            fileUrl: String, onFailed: () -> Unit = {}) {
-		if (fileUrl.isBlank() || !URLUtility.isValidURL(fileUrl)) {
-			onFailed()
-			return
-		}
-
-		val intent = Intent(ACTION_VIEW, fileUrl.toUri()).apply {
-			if (context !is Activity) {
-				addFlags(FLAG_ACTIVITY_NEW_TASK)
+	suspend fun openLinkInSystemBrowser(
+		context: Context, fileUrl: String, onFailed: () -> Unit = {}) {
+		withMainContext {
+			if (fileUrl.isBlank() || !URLUtility.isValidURL(fileUrl)) {
+				onFailed()
+				return@withMainContext
 			}
-		}
 
-		try {
-			context.startActivity(intent)
-		} catch (error: Exception) {
-			error.printStackTrace()
-			onFailed()
+			val intent = Intent(ACTION_VIEW, fileUrl.toUri()).apply {
+				if (context !is Activity) {
+					addFlags(FLAG_ACTIVITY_NEW_TASK)
+				}
+			}
+
+			try {
+				context.startActivity(intent)
+			} catch (error: Exception) {
+				error.printStackTrace()
+				onFailed()
+			}
 		}
 	}
 }
